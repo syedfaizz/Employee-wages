700986d720f6e1f484c3d2b6fea2bc10e160f4f8
syedfaizz <syedfaizz45@gmail.com>
2021-07-31 16:40:29 +0530

    [faisal] Add : Maxhrs-Monthly-wage

--- Maxhrs-Monthly-wage.cs
+++ Maxhrs-Monthly-wage.cs
@@ -2,42 +2,48 @@
 using System.Collections.Generic;
 using System.Text;
 namespace EmployeeWageComputation
+    namespace EmployeeWageComputation
 {
     class EmployeeWage
     {
         // constants
+        const int IS_PART_TIME = 1;
+        const int IS_FULL_TIME = 2;
         const int EMP_RATE_PER_HR = 20;
         const int MAX_WORKING_DAY = 20;
-
+        const int MAX_HRS_IN_MONTH = 100;
         // variables
-        int totalWage = 0;
-        int dailyWage = 0;
         int empHrs = 0;
-
+        int totalMonthlyWage = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
         /// <summary>
         /// This method is used to evaluate the monthly wage of an employee.
         /// </summary>
-        public void monthWage()
+        public void MonthWage()
         {
-            for (int i = 1; i <= MAX_WORKING_DAY; i++)
+            while (totalEmpHrs < MAX_HRS_IN_MONTH &&
+                    totalWorkingDays < MAX_WORKING_DAY)
             {
+                totalWorkingDays++;
                 Random rd = new Random();
                 int empCheck = rd.Next(0, 3);
                 switch (empCheck)
                 {
-                    case 2:
+                    case IS_FULL_TIME:
                         empHrs = 8;
                         break;
-                    case 1:
+                    case IS_PART_TIME:
                         empHrs = 4;
                         break;
                     default:
                         empHrs = 0;
                         break;
                 }
+                totalEmpHrs += empHrs;
             }
-            dailyWage = EMP_RATE_PER_HR * empHrs;
-            Console.WriteLine("Employee monthly wage:" + (totalWage + dailyWage));
+            totalMonthlyWage = totalEmpHrs * EMP_RATE_PER_HR;
+            Console.WriteLine("Total monthly wage:" + totalMonthlyWage);
         }
     }
 }
