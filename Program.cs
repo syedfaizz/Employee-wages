373c7d1b4f1024929ae516236a87f4bf76d54bb4
syedfaizz <syedfaizz45@gmail.com>
2021-07-31 14:30:55 +0530

    [faisal] Add : welcome

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employee_Wage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
